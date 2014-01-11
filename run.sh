058c3c682ed2343f50be171bf025f31bd6ca54c1
Evan Hazlett <ejhazlett@gmail.com>
2014-01-11 11:03:27 -0500

    added proxy headers for ssl

--- run.sh
+++ run.sh
@@ -62,6 +62,8 @@ cat << EOF >> $NGINX_CONF
       proxy_set_header X-Forwarded-Host \$host;
       proxy_set_header X-Real-IP \$remote_addr;
       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Scheme $scheme;
+      proxy_set_header X-Forwarded-Protocol $scheme;
     }
 
     location /console/ {
